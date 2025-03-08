538ca99aa329197948edb27083f80d288a4b77fa
jack <dfsgyg@se.sdf>
2025-03-08 18:50:35 +0800

    add metamask

--- mint.py
+++ mint.py
@@ -13,12 +13,12 @@ with open('mint.json', 'r', encoding='utf-8') as file:
 headers = {
     'Authorization': 'Bearer eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIweDUzNDAxYTVmMzlCNDQ5MThBRjk1ZDIxZTYzNEI2M0VlQzNlYzZlNWQiLCJzdWIiOiIweDA2NzUyY0Y5MTgyRmUwNTQ3NDI2QTlhRGU5ZkNmNjY4NzY1RTA3MjciLCJhdWQiOiJ0aGlyZHdlYi5jb20iLCJleHAiOjQ4OTQ5Mzc4MjUsIm5iZiI6MTc0MTMzNzgyNSwiaWF0IjoxNzQxMzM3ODI1LCJqdGkiOiJlN2UwOTcyZi1mN2YyLTQ4NmEtOTI1MC0yNDZmZTQ3NDM0NDYiLCJjdHgiOnsicGVybWlzc2lvbnMiOiJBRE1JTiJ9fQ.MHgzZWViMzk5ZjVkNDEzZmNiMjYzYmFlNzAyMGEyMGRmNjc5MTFlNjZiOTc1N2ZjNmZjMDJjNjYwOWM2ZTBjYjFkNzY3OGRkNjk3NDhmYmE2MzhlZWEyMGE3NGFkZmZhYTEzMDYxZGJmMjNmODc0OTFkNmVlZmExZGU2NGYyYzI3NzFi',
     'X-Backend-Wallet-Address': '0x06752cF9182Fe0547426A9aDe9fCf668765E0727',
-    'User-Agent': 'Apifox/1.0.0 (https://apifox.com)',
+
     'Content-Type': 'application/json',
     'Accept': '*/*',
     'Host': 'f9668c2b.engine-usw2.thirdweb.com',
     'Connection': 'keep-alive',
-    'Cookie': '_cfuvid=.8rry8oK72i.z8lcA244MszHGcd6n.3lBY12JTsPR9o-1741339656627-0.0.1.1-604800000'
+
 }
 
 # 执行 100 次
